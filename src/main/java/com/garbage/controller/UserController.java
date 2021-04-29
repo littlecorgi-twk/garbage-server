924ee8e6f5f7619407d4530442fea80367ce842e
egyptwang <1670142221@qq.com>
2021-04-29 18:27:55 +0800

    swaggerui修改为swagger-bootstrap-ui

--- src/main/java/com/garbage/controller/UserController.java
+++ src/main/java/com/garbage/controller/UserController.java
@@ -7,20 +7,20 @@ import com.garbage.pojo.User;
 import com.garbage.service.IFileService;
 import com.garbage.service.IUserService;
 import com.garbage.util.PhoneUtil;
-import com.garbage.util.PropertiesUtil;
 import com.google.common.collect.Maps;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import java.util.Enumeration;
 import java.util.Map;
 
 @Api(value = "用户相关操作")
