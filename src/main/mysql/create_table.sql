86a90d6fcb60774c95a069b152c690f62c5e544b
egyptwang <1670142221@qq.com>
2021-04-30 18:20:19 +0800

    更新MySQL建库代码

--- src/main/mysql/create_table.sql
+++ src/main/mysql/create_table.sql
@@ -1,28 +1,28 @@
 USE garbagecollection;
 
+-- auto-generated definition
 create table user
 (
-    id          int auto_increment,
+    id          int auto_increment
+        primary key,
     password    varchar(64)  null,
     name        varchar(64)  null,
     sex         varchar(2)   null,
     stage       varchar(64)  null,
     msg         varchar(64)  null,
     img         varchar(256) null,
-    photo       varchar(256) null,
+    phone       varchar(256) null,
     update_time timestamp    null,
-    create_time timestamp    null,
-    constraint user_pk
-        primary key (id)
+    create_time timestamp    null
 );
 
+-- auto-generated definition
 create table qq_user
 (
-    id          int auto_increment,
+    id          int auto_increment
+        primary key,
     qq_id       varchar(64) null,
     phone       char(11)    null,
     update_time timestamp   null,
-    create_time timestamp   null,
-    constraint qq_user_pk
-        primary key (id)
+    create_time timestamp   null
 );
